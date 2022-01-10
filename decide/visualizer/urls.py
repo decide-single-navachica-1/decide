2abc492effe2685c5bb4d1ea19a90b8b9233e436
ignrodflo <ignrodflo@alum.us.es>
2022-01-10 21:57:07 +0100

    Add new views
    add new views for all votings and add new chart to it

--- decide/visualizer/urls.py
+++ decide/visualizer/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import VisualizerView, VisualizerQuestion
+from .views import VisualizerView, VisualizerQuestion,  VisualizerAll
 
 
 urlpatterns = [
     path('<int:voting_id>/', VisualizerView.as_view()),
-    path('',VisualizerQuestion.as_view()),
+    path('',VisualizerAll.as_view()),
+    path('question/<int:question_id>/',VisualizerQuestion.as_view()),
 ]
--- decide/visualizer/views.py
+++ decide/visualizer/views.py
@@ -22,18 +22,44 @@ class VisualizerView(TemplateView):
         return context
 
 class VisualizerQuestion(TemplateView):
-    template_name = 'visualizer/all.html'
-
+    template_name = 'visualizer/question/visualizer.html'
+  
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        vid = kwargs.get('question_id', 0)
         
         try:
             res = []
-            r = mods.get('voting', params={'question_id': 1})
+            r = mods.get('voting', params={'question_id': vid})
             for ri in r :
                 res.append(json.dumps(ri))
             context['votings'] = res
         except:
+            raise Exception(res)
+
+       	return context
+        
+class VisualizerAll(TemplateView):
+    template_name = 'visualizer/all.html'
+  
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        try:
+            res = []
+            r = mods.get('voting')
+            for ri in r :
+                res.append(json.dumps(ri))
+            unstarted = 5
+            started = 0 
+            finished = 4
+            closed = 8
+            context['votings'] = res
+            context['unstarted'] = unstarted
+            context['started'] = started
+            context['finished'] = finished
+            context['closed'] = closed
+        except:
             raise Http404
 
        	return context
+
