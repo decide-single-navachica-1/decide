63695a4342f7b79105de3fa51e8f863016140c7b
javhidrod8 <48653257+javhidrod8@users.noreply.github.com>
2022-01-10 22:14:50 +0100

    CREACIÓN DE TESTS
    Añadidos los tests de vista y votación existente y no existente.

--- decide/visualizer/tests.py
+++ decide/visualizer/tests.py
@@ -1,3 +1,28 @@
-from django.test import TestCase
+from base.tests import BaseTestCase
+import visualizer.views as view
 
-# Create your tests here.
+class VisualizerTests(BaseTestCase):
+    data = ['visualizer/migrations/prueba.json', ]
+
+    def setUp(self):
+        super().setUp()
+
+    def tearDown(self):
+        super().tearDown()
+        
+    def test_vista_visualizer(self):
+       response = self.client.get('/visualizer/')
+       self.assertEqual(response.status_code, 200)
+
+    def test_votacion_existente(self):
+    response = self.client.get('/visualizer/1/')
+    self.assertEqual(response.status_code, 200)
+
+    def test_votacion_no_existente(self):
+        response = self.client.get('/visualizer/999/')
+        self.assertEqual(response.status_code, 404)
+        
+  
+        
+
+    
