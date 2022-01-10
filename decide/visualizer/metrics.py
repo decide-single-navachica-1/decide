0f5217034c068cd064f50a4c5dad2ddce0950405
Franbr31 <62068127+Franbr31@users.noreply.github.com>
2022-01-10 21:43:14 +0100

    MÉTRICAS
    añadido el número de abstenciones para una votación

--- decide/visualizer/metrics.py
+++ decide/visualizer/metrics.py
@@ -24,3 +24,8 @@ def votingComparator(v1_id, v2_id):
     v1 = votesOfVoting(v1_id)
     v2 = votesOfVoting(v2_id)
     return (v1/v2)*100
+
+def abstentions(v_id):
+    v = votesOfVoting(v_id)
+    c = Census.objects.filter(voting_id = v_id).count()
+    return c - v
