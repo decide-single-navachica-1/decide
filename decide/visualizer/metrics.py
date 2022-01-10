da8f74ab1a655b50680d0aef98a372bfaf1199d5
Franbr31 <62068127+Franbr31@users.noreply.github.com>
2022-01-10 21:10:29 +0100

    MÉTRICAS
    subidas las métricas para las votaciones iniciadas, finalizadas y cerradas

--- decide/visualizer/metrics.py
+++ decide/visualizer/metrics.py
@@ -11,3 +11,12 @@ def votesOfVoting(v_id):
 def unstartedVotings():
     return Voting.objects.filter(start_date__isnull=True).count()
 
+def startedVotings():
+    return Voting.objects.filter(start_dateisnull=False).count()
+
+def finishedVotings():
+    return Voting.objects.filter(end_dateisnull=False, tallyisnull=False).count()
+
+def closedVotings():
+    return Voting.objects.filter(end_dateisnull=False, tally__isnull=True).count()
+
